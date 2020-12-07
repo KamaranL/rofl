7fecc771b897ce755c1bce52fd4081f8cf56969a
Kamaran Layne <kamaran.layne@gmail.com>
2020-12-07 12:44:34 -0500

    refactored card design and started tablet styling

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 const cards = {
   ringOfFireLite: {
     values: {
-      1: "Waterfall &mdash; Everyone should keep drinking until the person who picked the card stops.",
-      2: "You &mdash; You pick someone that has to drink.",
-      3: "Me &mdash; You must drink.",
-      4: "Whores &mdash; All girls must drink.",
-      5: "Thumb Master &mdash; When you put your thumb on the table, everyone must follow and whomever is last must drink. You are thumb master until someone else gets this number.",
-      6: "Dicks &mdash; All guys must drink.",
-      7: "Heaven &mdash; Point your finger to the sky, whoever is last must drink.",
-      8: "Mate &mdash; Pick someone to drink with you. He/She is now your drinking buddy and must always drink when you drink.",
-      9: "Rhyme &mdash; Pick a word, such as fog, and as you go through the circle of players, everyone has to say a word that rhymes with fog, until someone messes up. Whoever messes up will have to drink.",
-      10: "Categories &mdash; Pick a category, such as cereal brands, and as you go through the circle of players, everyone has to name a brand of cereal, until someone messes up.  Whoever messes up will have to drink.",
+      1: "<h4 class=\"text-center\">Waterfall</h4>Everyone should keep drinking until the person who picked the card stops.",
+      2: "<h4 class=\"text-center\">You</h4>You pick someone that has to drink.",
+      3: "<h4 class=\"text-center\">Me</h4>You must drink.",
+      4: "<h4 class=\"text-center\">Whores</h4>All girls must drink.",
+      5: "<h4 class=\"text-center\">Thumb Master</h4>When you put your thumb on the table, everyone must follow and whomever is last must drink. You are thumb master until someone else gets this number.",
+      6: "<h4 class=\"text-center\">Dicks</h4>All guys must drink.",
+      7: "<h4 class=\"text-center\">Heaven</h4>Point your finger to the sky, whoever is last must drink.",
+      8: "<h4 class=\"text-center\">Mate</h4>Pick someone to drink with you. He/She is now your drinking buddy and must always drink when you drink.",
+      9: "<h4 class=\"text-center\">Rhyme</h4>Pick a word, such as fog, and as you go through the circle of players, everyone has to say a word that rhymes with fog, until someone messes up. Whoever messes up will have to drink.",
+      10: "<h4 class=\"text-center\">Categories</h4>Pick a category, such as cereal brands, and as you go through the circle of players, everyone has to name a brand of cereal, until someone messes up.  Whoever messes up will have to drink.",
       J:
-        "Make a Rule &mdash; You can make up any rule, within reason, that everyone has to follow- including yourself. Whoever disobeys this rule must drink.",
+        "<h4 class=\"text-center\">Make a Rule</h4>You can make up any rule, within reason, that everyone has to follow- including yourself. Whoever disobeys this rule must drink.",
       K:
-        "Pour! &mdash; Pour any amount of your drink into a cup or glass and set it in the middle of the table. Whoever draws the 4th King, must drink the entire concoction that is in the cup.",
+        "<h4 class=\"text-center\">Pour!</h4>Pour any amount of your drink into a cup or glass and set it in the middle of the table. Whoever draws the 4th King, must drink the entire concoction that is in the cup.",
       Q:
-        "Questions &mdash; Go through the circle of players asking the player next in the circle a question. It does not matter what the question is, first person to not answer or to not ask a question must drink.",
+        "<h4 class=\"text-center\">Questions</h4>Go through the circle of players asking the player next in the circle a question. It does not matter what the question is, first person to not answer or to not ask a question must drink.",
     },
     kingCount: function () {
       let current = {};
